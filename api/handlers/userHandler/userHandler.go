0d985657921d6a082d4b00a8b9cd269a89c454b6
diegorezm <dmourarezende@gmail.com>
2024-02-21 18:37:28 -0300

    encription

--- api/handlers/userHandler/userHandler.go
+++ api/handlers/userHandler/userHandler.go
@@ -1,7 +1,10 @@
 package userhandler
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
 	userModel "github/diegorezm/school_go/api/models/user"
 	"log"
@@ -12,7 +15,48 @@ const (
 	SELECT_QUERY_BY_ID    = "SELECT id,name,email,password FROM school.users WHERE id= ?"
 	SELECT_QUERY_BY_EMAIL = "SELECT id,name,email,password FROM school.users WHERE email= ?"
 	INSERT_QUERY          = "insert into users (name, email, password) values (?, ?, ?)"
+	SECRET_KEY            = "abc&1*~#^2^#s0^=)^^7%b34"
 )
+
+// i have no idea of what is happening here
+// stole everything from https://blog.logrocket.com/learn-golang-encryption-decryption/
+
+var bytes = []byte{35, 46, 57, 24, 85, 35, 24, 74, 87, 35, 88, 98, 66, 32, 14, 05}
+func Encode(b []byte) string {
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+func Decode(s string) []byte {
+ data, err := base64.StdEncoding.DecodeString(s)
+ if err != nil {
+  panic(err)
+ }
+ return data
+} 
+
+func Encrypt(text, MySecret string) (string, error) {
+	block, err := aes.NewCipher([]byte(MySecret))
+	if err != nil {
+		return "", err
+	}
+	plainText := []byte(text)
+	cfb := cipher.NewCFBEncrypter(block, bytes)
+	cipherText := make([]byte, len(plainText))
+	cfb.XORKeyStream(cipherText, plainText)
+	return Encode(cipherText), nil
+}
+
+func Decrypt(text, MySecret string) (string, error) {
+	block, err := aes.NewCipher([]byte(MySecret))
+	if err != nil {
+		return "", err
+	}
+	cipherText := Decode(text)
+	cfb := cipher.NewCFBDecrypter(block, bytes)
+	plainText := make([]byte, len(cipherText))
+	cfb.XORKeyStream(plainText, cipherText)
+	return string(plainText), nil
+}
 
 type UserHandler struct {
 	connection *sql.DB
@@ -31,6 +75,7 @@ func (uh UserHandler) GetAllUsers() ([]userModel.UserModel, error) {
 	var users []userModel.UserModel
 	for rows.Next() {
 		var user userModel.UserModel
+		// probably shouldn't return the password here,
 		if err := rows.Scan(&user.Id, &user.Name, &user.Email, &user.Password); err != nil {
 			return nil, fmt.Errorf("Error while trying to scan users.")
 		}
@@ -42,21 +87,35 @@ func (uh UserHandler) GetAllUsers() ([]userModel.UserModel, error) {
 func (uh UserHandler) Login(email, password string) (userModel.UserModel, error) {
 	row := uh.connection.QueryRow(SELECT_QUERY_BY_EMAIL, email)
 	var user userModel.UserModel
-	err := row.Scan(&user.Id, &user.Name,&user.Email, &user.Password)
+	err := row.Scan(&user.Id, &user.Name, &user.Email, &user.Password)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return userModel.UserModel{}, fmt.Errorf("No user found with this email!")
 		} else {
-      log.Printf("Error scanning row: %v", err)
+			log.Printf("Error scanning row: %v", err)
 			return userModel.UserModel{}, fmt.Errorf("Error while trying to fetch user!")
 		}
+	}
+	password_hash , err:= Decrypt(user.Password, SECRET_KEY)
+  if err != nil {
+		log.Print(err.Error())
+		return userModel.UserModel{}, fmt.Errorf("Error while decoding the password!")
+  }
+	if password != password_hash {
+		return userModel.UserModel{}, fmt.Errorf("Wrong password!")
 	}
 	return user, nil
 }
 
 func (uh UserHandler) Register(newUser userModel.UserModel) (userModel.UserModel, error) {
-	result, err := uh.connection.Exec(INSERT_QUERY, newUser.Name, newUser.Email, newUser.Password)
+	password_hash , err:= Encrypt(newUser.Password, SECRET_KEY)
+  if err != nil {
+		log.Print(err.Error())
+		return userModel.UserModel{}, fmt.Errorf("Error while encoding the password!")
+  }
+	result, err := uh.connection.Exec(INSERT_QUERY, newUser.Name, newUser.Email, &password_hash)
 	if err != nil {
+		log.Print(err.Error())
 		return userModel.UserModel{}, fmt.Errorf("Error while inserting user!")
 	}
 
